3483675795abcd8a14677c2e0385d76a4043ce21
S-Ebadi <said.ebadi@chasacademy.se>
2025-10-03 11:51:43 +0200

    ny lektion med CJ

---
+++ Workshop3-okt/lib_demo.py
@@ -0,0 +1,33 @@
+from random import random, randint, choice, shuffle
+import time
+import json
+import tkinter as tk
+
+'''
+dice_cast = randint(1, 6)
+print(dice_cast)
+
+names = ["Alice", "Bob", "Charlie", "Diana"]
+random_name = choice(names)
+print(random_name)
+
+shuffle(names)
+
+
+for name in names:
+    print("Getting name...")
+    time.sleep(2)
+    print(f"Name: {name}")
+
+'''
+
+root = tk.Tk()
+root.title("Demo")
+
+def hello():
+    print("Hello, Tkinter!")
+
+btn = tk.Button(root, text="Click Me", command=hello)
+btn.pack()
+
+root.mainloop()
